fdb337ab99be2d4726429daf032a1206a569a475
himanshusanecha <52252631+himanshusanecha@users.noreply.github.com>
2020-05-05 02:47:41 +0530

    updated colorgame.js

--- colorgame.js
+++ colorgame.js
@@ -1,8 +1,48 @@
-var colors = generaterandomcolors(6);
+var number = 6;
+var colors = generaterandomcolors(number);
 var squares = document.querySelectorAll(".square");
 var pickedcolor = pickcolor();
 var colordisplay = document.getElementById("display");
 var message = document.getElementById("message");
+var reset = document.getElementById("reset");
+var h1 = document.querySelector("h1");
+var easybtn = document.querySelector("#easybtn");
+var hardbtn = document.querySelector("#hardbtn");
+
+
+
+easybtn.addEventListener("click",function() {
+	easybtn.classList.add("selected");
+	hardbtn.classList.remove("selected");
+	number = 3;
+	colors = generaterandomcolors(number);
+	pickedcolor = pickcolor();
+	colordisplay.textContent = pickedcolor;
+	for(var i=0;i<squares.length;i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		}
+		else{
+			squares[i].style.display = "none";
+		}
+	}
+	reset.textContent = "new colors";
+})
+hardbtn.addEventListener("click",function() {
+	easybtn.classList.remove("selected");
+	hardbtn.classList.add("selected");
+	number = 6;
+	h1.style.backgroundColor = "#232323";
+	reset.textContent = "new colors";
+	colors = generaterandomcolors(number);
+	pickedcolor = pickcolor();
+	colordisplay.textContent = pickedcolor;
+	for(var i=0;i<squares.length;i++){
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+	}
+	reset.textContent = "new colors"; 
+})
 colordisplay.textContent = pickedcolor;
 for(var i=0;i<squares.length;i++){
 	squares[i].style.backgroundColor = colors[i];
@@ -12,6 +52,8 @@ for(var i=0;i<squares.length;i++){
 		if(clickedcolor === pickedcolor){
 			message.textContent = "Correct";
 			changecolors(clickedcolor);
+			reset.textContent = "play again?";
+			h1.style.backgroundColor = pickedcolor;
 		} else {
 			this.style.backgroundColor = "#232323";
 			message.textContent = "Try Again";
@@ -43,3 +85,13 @@ function randomcolor(){
 	return "rgb(" + r + ", " + g + ", " + b +")";
 
 }
+reset.addEventListener("click", function(){
+	h1.style.backgroundColor = "#232323";
+	reset.textContent = "new colors";
+	colors = generaterandomcolors(number);
+	pickedcolor = pickcolor();
+	colordisplay.textContent = pickedcolor;
+	for(var i=0;i<squares.length;i++){
+		squares[i].style.backgroundColor = colors[i];
+	} 
+});
